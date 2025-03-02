95c5fe64b469b3d509b4cc819fbe9c9c02b0f3ea
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-03-02 21:30:46 +0000

    step 1 & 2 complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,32 @@
 class SessionsController < ApplicationController
   def new
+    @users = User.all
   end
   
   def create
     # TODO: authenticate user
-    flash["notice"] = "Nope."
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcom, #{@user["first_name"]}."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    flash["notice"] = "Goodbye"
+    session["user_id"] = nil
     redirect_to "/login"
   end
+
+
+
 end
